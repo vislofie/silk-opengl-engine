c0bfdbc8f90b96d8d36fa5a6b365611cf3ca4a05
Александр Карпов <karpovai@trueconf.ru>
2025-02-13 00:22:34 +0300

    init

---
+++ Program.cs
@@ -0,0 +1,153 @@
+﻿using System.Drawing;
+using Silk.NET.Input;
+using Silk.NET.Maths;
+using Silk.NET.OpenGL;
+using Silk.NET.Windowing;
+
+class Program
+{
+    private static IWindow _window;
+
+    private static GL _gl;
+    private static uint _vao;
+    private static uint _vbo;
+    private static uint _ebo;
+    private static uint _sProgram;
+
+    public static void Main(string[] args)
+    {
+        WindowOptions options = WindowOptions.Default with
+        {
+            Size = new Vector2D<int>(800, 600),
+            Title = "Hello, window!"
+        };
+
+        _window = Window.Create(options);
+        _window.Load += OnLoad;
+        _window.Render += OnRender;
+        _window.Run();
+        
+    }
+
+    private static unsafe void OnLoad()
+    {
+        IInputContext input = _window.CreateInput();
+        for (int i = 0; i < input.Keyboards.Count; i++)
+            input.Keyboards[i].KeyDown += KeyDown;
+
+        _gl = _window.CreateOpenGL();
+
+        _vao = _gl.GenVertexArray();
+        _gl.BindVertexArray(_vao);
+
+        float[] vertices =
+        {
+            0.5f,  0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+           -0.5f, -0.5f, 0.0f,
+           -0.5f,  0.5f, 0.0f,
+        };
+
+        _vbo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ArrayBuffer, _vbo);
+
+        fixed (float* buf = vertices)
+        {
+            _gl.BufferData(BufferTargetARB.ArrayBuffer, (nuint) (vertices.Length * sizeof(float)), buf, BufferUsageARB.StaticDraw);
+        }
+
+        uint[] indices =
+        {
+            0u, 1u, 3u,
+            1u, 2u, 3u
+        };
+
+        _ebo = _gl.GenBuffer();
+        _gl.BindBuffer(BufferTargetARB.ElementArrayBuffer, _ebo);
+
+        fixed (uint* buf = indices)
+        {
+            _gl.BufferData(BufferTargetARB.ElementArrayBuffer, (nuint) (indices.Length * sizeof(uint)), buf, BufferUsageARB.StaticDraw);
+        }
+
+        const string vertexCode = @"
+        #version 330 core
+        
+        layout (location = 0) in vec3 aPosition;
+        
+        void main()
+        {
+            gl_Position = vec4(aPosition, 1.0);
+        }";
+
+        const string fragmentCode = @"
+        #version 330 core
+        
+        out vec4 out_color;
+        
+        void main()
+        {
+            out_color = vec4(1.0, 0.5, 0.2, 1.0);
+        }";
+
+        uint vertexShader = _gl.CreateShader(ShaderType.VertexShader);
+        _gl.ShaderSource(vertexShader, vertexCode);
+        _gl.CompileShader(vertexShader);
+        _gl.GetShader(vertexShader, ShaderParameterName.CompileStatus, out int vStatus);
+        if (vStatus != (int) GLEnum.True)
+        {
+            throw new Exception("Vertex shader failed to compile!\n" + _gl.GetShaderInfoLog(vertexShader));
+        }
+
+        uint fragmentShader = _gl.CreateShader(ShaderType.FragmentShader);
+        _gl.ShaderSource(fragmentShader, fragmentCode);
+        _gl.CompileShader(fragmentShader);
+        _gl.GetShader(fragmentShader, GLEnum.CompileStatus, out int fStatus);
+        if (fStatus != (int) GLEnum.True)
+        {
+            throw new Exception("Fragment shader failed to compile!\n" + _gl.GetShaderInfoLog(fragmentShader));
+        }
+        
+        _sProgram = _gl.CreateProgram();
+
+        _gl.AttachShader(_sProgram, vertexShader);
+        _gl.AttachShader(_sProgram, fragmentShader);
+
+        _gl.LinkProgram(_sProgram);
+
+        _gl.GetProgram(_sProgram, GLEnum.LinkStatus, out int lStatus);
+        if (lStatus != (int) GLEnum.True)
+        {
+            throw new Exception("Shader program failed to link!" + _gl.GetProgramInfoLog(_sProgram));
+        }
+
+        _gl.DetachShader(_sProgram, vertexShader);
+        _gl.DetachShader(_sProgram, fragmentShader);
+        _gl.DeleteShader(vertexShader);
+        _gl.DeleteShader(fragmentShader);
+
+        const uint positionLoc = 0;
+        _gl.EnableVertexAttribArray(positionLoc);
+        _gl.VertexAttribPointer(positionLoc, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), (void*)0);
+
+        _gl.BindVertexArray(0);
+        _gl.BindBuffer(GLEnum.ArrayBuffer, 0);
+        _gl.BindBuffer(GLEnum.ElementArrayBuffer, 0);
+    }
+
+    private static unsafe void OnRender(double deltaTime)
+    {
+        _gl.ClearColor(Color.CornflowerBlue);
+        _gl.Clear(ClearBufferMask.ColorBufferBit);
+
+        _gl.BindVertexArray(_vao);
+        _gl.UseProgram(_sProgram);
+        _gl.DrawElements(PrimitiveType.Triangles, 6, DrawElementsType.UnsignedInt, (void*)0);
+    }
+
+    private static void KeyDown(IKeyboard keyboard, Key key, int keyCode)
+    {
+        if (key == Key.Escape)
+            _window.Close();
+    }
+}
